18c631c4ad32a96a8144975b02d0603d8c08e529
manoranjan86 <57694813+manoranjan86@users.noreply.github.com>
2019-11-25 09:55:50 +0530

    script

---
+++ app.component.ts
@@ -0,0 +1,9 @@
+﻿     
+import{Component } from '@angular/core' 
+ @Component({  
+    selector: 'my-component',
+    template: '<employee-view>employeelist</employee-view>'
+})
+export class AppComponent {
+
+}
---
+++ app.module.ts
@@ -0,0 +1,29 @@
+﻿
+
+import {NgModule} from "@angular/core" ;
+import {NgForm} from '@angular/forms';
+import {FormsModule} from '@angular/forms';
+import { AppComponent } from "./app.component" 
+import { EmployeeComponent } from "./components/employee/employee.component" 
+import { BrowserModule } from '@angular/platform-browser' 
+import { EmployeeDummyLogic, EmployeeRealLogic, EmployeeLogic } from './logic/employee.dummy.logic'
+import { EmployeeListComponent} from './components/employee/employee.list.component'
+import { EmployeeAddComponent } from './components/employee/employee.add.component'
+import { RatingComponent} from './components/employee/rating.components'
+import { namepipe} from './components/employee/name.pipe'
+import{highlight} from './Directive/highlight.directive'
+@NgModule({
+    imports: [BrowserModule,FormsModule],
+    declarations: [AppComponent, EmployeeComponent, EmployeeAddComponent,
+                     EmployeeListComponent, RatingComponent,namepipe,highlight],
+    bootstrap: [AppComponent],
+    providers: [
+        {  
+        provide :EmployeeLogic,
+        useClass:EmployeeDummyLogic 
+        }
+    ]
+})
+export class AppModule{
+
+}
